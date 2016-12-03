bbdd84ccbe3d3cb2aa932bbbe4895a67fdbfca23
italocegatta <italocegatta@gmail.com>
2016-12-03 13:47:48 -0200

    Alteração de >= para > para seleção de competidores e borda

--- R/01_border.R
+++ R/01_border.R
@@ -1,17 +1,17 @@
 #' Available trees for measure and compute index
-#' 
+#'
 #' @export
-#' 
+#'
 available_tree <- function(x, y, border) {
   ifelse(
     '&'(
       '&'(
-        x <= max(x)-border,
-        x >= min(x)+border
+        x < max(x)-border,
+        x > min(x)+border
       ),
       '&'(
-        y <= max(y)-border,
-        y >= min(y)+border
+        y < max(y)-border,
+        y > min(y)+border
       )
     ),
     TRUE, FALSE
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-comp3
+# comp3: an R package for competition index in individuals trees
+
+### Installation and Documentation
+
+```
+# install.packages("devtools")
+devtools::install_github("italocegatta/comp3")
+```
+
