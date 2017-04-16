c5e1688415a61a98aadd6772fd99771a8ad771f7
italocegatta <italocegatta@gmail.com>
2017-04-16 17:20:36 -0300

    Document eucalyptus

--- R/eucalyptus.R
+++ R/eucalyptus.R
@@ -1,5 +1,16 @@
+#' Eucalyptus data set
+#'
 #' A amesure plot of Eucalyptus
 #'
-#' @examples
+#' @format A data frame with eight variables:
+#' \itemize{
+#'   \item{\code{site}}{: Three different sites}
+#'   \item{\code{area}}{: Area of plot in squart meters}
+#'   \item{\code{age}}{: Age of tree in years}
+#'   \item{\code{tree}}{: Number of tree on the plot}
+#'   \item{\code{dbh}}{: Diameter of trees at 1.3 meters }
+#'   \item{\code{h}}{: Height of trees in meters}
+#'   \item{\code{cod}}{: Attributes of trees}
+#' }
 #'
 "eucalyptus"
